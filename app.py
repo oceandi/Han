63b432a48e541d27c3d6e1cb1ddbe3d1e61279e9
oceandi <126319987+oceandi@users.noreply.github.com>
2023-12-03 16:20:21 +0300

    1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import joblib
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 # Modeli yükle
 model = joblib.load('text_classifier.model')
@@ -13,10 +13,10 @@ def predict():
     predictions = model.predict([data['text']])
     return jsonify({'prediction': predictions[0]})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
 @app.route('/')
 def home():
     return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
